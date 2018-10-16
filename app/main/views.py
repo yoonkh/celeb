08236856cc299dc3d1875067693ddfe8bfeeba54
kyeong-han yoon <zizou0812@gmail.com>
2018-10-16 13:50:08 +0900

    test

--- app/main/views.py
+++ app/main/views.py
@@ -13,26 +13,26 @@ def index():
 @main.route('/crawl')
 def crawl():
     sports_dong_a.main() # 스포츠동아
-    tv_report.main() # TV리포트
-    sports_seoul.main() # 스포츠서울
-    sports_hangook.main() # 스포츠한국
-    sports_world.main() # 스포츠월드
-    joynews_inews.main() # 조이뉴스
-    sbs.main() # SBS연예스포츠
-    enews24.main() # enews24
-    isplus.main() # 일간스포츠
-    newsen.main() # 뉴스엔
-    mydaily.main() # 마이데일리
-    star_news.main() # 스타뉴스
-    sports_khan.main() # 스포츠경향
-    sports_chosun.main() # 스포츠조선
-    sports_today.main() # 스포츠투데이
-    xports_news.main() # 엑스포츠뉴스
-    mbc_sports_plus.main() # 엠스플뉴스
-    mk_sports.main() # MK스포츠
-    # osen.main() # OSEN
-    spotv_news.main() # SPOTV뉴스
-    ten_asia.main() # 텐아시아
-    tv_daily.main() # 티브이데일리
-    heraldpop.main() # 헤럴드팝
+    # tv_report.main() # TV리포트
+    # sports_seoul.main() # 스포츠서울
+    # sports_hangook.main() # 스포츠한국
+    # sports_world.main() # 스포츠월드
+    # joynews_inews.main() # 조이뉴스
+    # sbs.main() # SBS연예스포츠
+    # enews24.main() # enews24
+    # isplus.main() # 일간스포츠
+    # newsen.main() # 뉴스엔
+    # mydaily.main() # 마이데일리
+    # star_news.main() # 스타뉴스
+    # sports_khan.main() # 스포츠경향
+    # sports_chosun.main() # 스포츠조선
+    # sports_today.main() # 스포츠투데이
+    # xports_news.main() # 엑스포츠뉴스
+    # mbc_sports_plus.main() # 엠스플뉴스
+    # mk_sports.main() # MK스포츠
+    # # osen.main() # OSEN
+    # spotv_news.main() # SPOTV뉴스
+    # ten_asia.main() # 텐아시아
+    # tv_daily.main() # 티브이데일리
+    # heraldpop.main() # 헤럴드팝
     return '<h1>Success - Crawling</h1>'
--- app/text_cleaner.py
+++ app/text_cleaner.py
@@ -7,4 +7,5 @@ def clean_text(text):
     cleaned_text = re.sub('[a-zA-Z]', '', text)
     cleaned_text = re.sub('[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]',
                           '', cleaned_text)
+    cleaned_text = re.sub('^\s+[0-9]+\s+[0-9]+\s+[0-9]*\s', '', cleaned_text)
     return cleaned_text
